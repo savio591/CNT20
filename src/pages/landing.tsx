056efaed485c337cf7afc97a82932c31135578f5
savio591 <savio591@hotmail.com>
2020-10-25 18:40:43 +0000

    savio591_landing-page added

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,56 +1,45 @@
 import React from 'react';
-import { FiMap, FiUser, FiLogIn } from 'react-icons/fi';
+import { FiUser, FiLogIn } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 import '../styles/pages/landing.css';
 
 
-import logoImg from '../../src/images/lanLogo.svg';
+import logoLogin from '../../src/images/cnt_logo_login.png';
 
 function Landing() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="CNT" />        
+    return (
+        <div id="page-landing">
+            <div className="content-wrapper">
+                <img src={logoLogin} alt="CNT" className="logo_login" />
 
-        <main>
+                <div className="ct_login">
+                    <div>
+                        <p>Nome de Usuário</p>
+                        <label htmlFor="image[]" className="new-image">
+                            <FiUser size={32} color="#003771" />
+                            <input></input>
+                        </label>
 
-          <h1>Pesquisa CNT de Rodovias</h1>
-          <p>Sinalização e à Geometria da Via.</p>
-        </main>
-        <div className="location">
-
-          
-          <div>
-
-          </div>
-
-
-          <div>
-            <label htmlFor="image[]" className="new-image">
-              <FiUser size={32} color="#003771" />
-              <input></input>
-            </label>
-            
-
-            <label htmlFor="image[]" className="new-image">
-              <FiLogIn size={32} color="#003771" />
-              <input></input>
-            </label>
-            
-
-          </div>
+                        <p>Senha</p>
+                        <label htmlFor="image[]" className="new-image">
+                            <FiLogIn size={32} color="#003771" />
+                            <input></input>
+                        </label>
 
 
+                    </div>
+
+
+                </div>
+
+                <Link to="/cnt" className="enter-app">
+                <p>Entrar</p>
+                </Link>
+            </div>
         </div>
-        
-        <Link to="/cnt" className="enter-app">
-          <FiMap size={26} color="#FFF" />
-        </Link>
-      </div>
-    </div>
 
-  );
+    );
 }
 
 export default Landing;
